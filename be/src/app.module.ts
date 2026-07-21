78fa721d4805e2d4a9b474f3af53c1626d522c34
Ly Duc Tu <direct.ldt@gmail.com>
2026-07-21 16:51:23 +0700

    configure environment

--- be/src/app.module.ts
+++ be/src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+  import { Module } from '@nestjs/common';
+  import { ConfigModule } from '@nestjs/config';
+  import { AppController } from './app.controller';
+  import { AppService } from './app.service';
 
-@Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}
+  @Module({
+    imports: [
+      ConfigModule.forRoot({
+        isGlobal: true,
+      }),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
+  })
+  export class AppModule {}
 
